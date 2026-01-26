47f76f65a9d9bc9d178e9a83469897ec25425ea2
Ryan <ryanyuanzhehan@gmail.com>
2026-01-27 01:55:47 +0800

    backup

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,22 @@
 // 颜色渐变
 function startColorPulse() {
+    // 获取根和元素
     const root = document.documentElement;
-    const color1 = "rgb(12, 87, 226)";
-    const color2 = "rgb(104, 18, 234)";
+    const color1 = root.getPropertyValue('--bg-color-1').trim();
+    const color2 = root.getPropertyValue('--bg-color-2').trim();
 
+    // 代理变量
     const colorProxy = { color1, color2 };
 
     gsap.to(colorProxy, {
         color1: color2,
         color2: color1,
         duration: 3,
+        // 无限循环
         repeat: -1,
         yoyo: true,
         ease: "sine.inOut",
+        // 应用到变量
         onUpdate: () => {
             root.style.setProperty('--bg-color-1', colorProxy.color1);
             root.style.setProperty('--bg-color-2', colorProxy.color2);
@@ -20,5 +24,5 @@ function startColorPulse() {
     });
 }
 
-// 初始化
+// 启动
 startColorPulse();
